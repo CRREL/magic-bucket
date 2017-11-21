de4147c79ad3e867defebac3eda26cfc2404b7b5
Pete Gadomski <pete.gadomski@gmail.com>
2017-11-21 06:33:22 -0700

    Update test to actually work

--- test/slack_test.py
+++ test/slack_test.py
@@ -2,7 +2,7 @@ import os
 
 from magic_bucket import Slack
 
-slack = Slack(os.environ["SLACK_TOKEN"], "pdal-translate", ":pdal:")
+slack = Slack(os.environ["SLACK_TOKEN"])
 print slack.info("Information message")
 print slack.success("Success message")
 print slack.fail("Failure message")
